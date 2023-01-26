c89d6792659979ebb272f41f5f26db44e626b516
readberg <wayreadberg@gmail.com>
2023-01-26 16:20:13 +0300

    init: new project

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+
+<!-- Форма авторизации-->
+
+<form action="" method="">
+    <label>Логин</label>
+</form>
+
+</body>
+</html>
