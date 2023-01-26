2b57aefa4e5cd8c991403a7d2d4e0fc590917a07
readberg <wayreadberg@gmail.com>
2023-01-26 20:50:39 +0300

    feat: add data in json file

--- vendor/signup.php
+++ vendor/signup.php
@@ -9,9 +9,19 @@ $passwordConfirm = $_POST['password_confirm'];
 $email = $_POST['email'];
 $name = $_POST['name'];
 
+$jsonArray = [];
+
 if ($password !== $passwordConfirm){
     $_SESSION['passwordCorrect'] = '*Passwords do not match';
     header('Location: ../register.php');
+} else {
+    $jsonArray['login'] = $login;
+    $jsonArray['password'] = $password;
+    $jsonArray['email'] = $email;
+    $jsonArray['name'] = $name;
+    file_put_contents('../data.json', json_encode($jsonArray, JSON_FORCE_OBJECT), FILE_APPEND);
+
+    header('Location: ../');
 }
 
 ?>
